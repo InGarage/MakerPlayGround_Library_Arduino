d7dcdf7324000245fc56c6991246a24d380adcd9
Nuntipat Narkthong <nuntipat@gmail.com>
2018-05-03 01:08:59 +0700

    fix bug on .h on 7seg

--- src/MP_MakerPlayground7Seg.h
+++ src/MP_MakerPlayground7Seg.h
@@ -15,8 +15,9 @@ public:
 	void off();
 
 private:
+	void sendInvalid();
     void showFloat(double num);
-    void sendData(int num, uint8_t dot);
+    void sendData(int num, uint8_t dot, bool negative);
 
 	SendOnlySoftwareSerial serial;
 };
