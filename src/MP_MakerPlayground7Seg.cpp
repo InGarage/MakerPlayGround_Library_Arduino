7e839085e0c6bd9ea2fe41ac3dfdd0d6978239f3
Nuntipat Narkthong <nuntipat@gmail.com>
2018-05-03 01:33:38 +0700

    fix 7 seg

--- src/MP_MakerPlayground7Seg.cpp
+++ src/MP_MakerPlayground7Seg.cpp
@@ -23,39 +23,39 @@ void MP_MakerPlayground7Seg::sendData(int num, uint8_t dot, bool negative)
     }
 
     // send
-    serial.print(0x7f);
-    serial.print(3);
+    serial.print((byte)0x7f);
+    serial.print((byte)3);
     if (negative) {
-        serial.print(0b01000000);
+        serial.print((byte)0b01000000);
         for (int i=2; i>=0; i--) 
         {
             if (dot && (1 << i))
-                serial.print(tmp[i] | 0b10000000);
+                serial.print((byte)(digits[tmp[i]] | 0b10000000));
             else
-                serial.print(tmp[i]);
+                serial.print((byte)(digits[tmp[i]]));
         }
     } else {
         for (int i=3; i>=0; i--) 
         {
             if (dot && (1 << i))
-                serial.print(tmp[i] | 0b10000000);
+                serial.print((byte)(digits[tmp[i]] | 0b10000000));
             else
-                serial.print(tmp[i]);
+                serial.print((byte)(digits[tmp[i]]));
         }
     }
-    serial.print(0);
+    serial.print((byte)0);
 }
 
 void MP_MakerPlayground7Seg::sendInvalid()
 {
-    serial.print(0x7f);
-    serial.print(3);
-    serial.print(0b01000000);
-    serial.print(0b01000000);
-    serial.print(0b01000000);
-    serial.print(0b01000000);
-    serial.print(0b00000000);
-    serial.print(0);
+    serial.print((byte)0x7f);
+    serial.print((byte)3);
+    serial.print((byte)0b01000000);
+    serial.print((byte)0b01000000);
+    serial.print((byte)0b01000000);
+    serial.print((byte)0b01000000);
+    serial.print((byte)0b00000000);
+    serial.print((byte)0);
 }
 
 void MP_MakerPlayground7Seg::showFloat(double num)
@@ -98,12 +98,12 @@ void MP_MakerPlayground7Seg::showData(double value)
 
 void MP_MakerPlayground7Seg::off()
 {
-    serial.print(0x7f);
-    serial.print(0);
-    serial.print(0b00000000);
-    serial.print(0b00000000);
-    serial.print(0b00000000);
-    serial.print(0b00000000);
-    serial.print(0b00000000);
-    serial.print(0);
+    serial.print((byte)0x7f);
+    serial.print((byte)0);
+    serial.print((byte)0b00000000);
+    serial.print((byte)0b00000000);
+    serial.print((byte)0b00000000);
+    serial.print((byte)0b00000000);
+    serial.print((byte)0b00000000);
+    serial.print((byte)0);
 }
